7d926dd76027bbd622501b2c11bb4c7a8bb02f5d
yokubo <utsue.38@gmail.com>
2018-06-22 23:16:45 +0900

    generate ResultActivity

---
+++ Janken/app/src/main/java/com/first/c0tt0n/janken/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.first.c0tt0n.janken
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_result)
+  }
+}
