3085ac48c97821be6faab9271e5d04090c081bf2
yokubo <utsue.38@gmail.com>
2018-06-22 23:07:59 +0900

    create main screen

--- Janken/app/src/main/java/com/first/c0tt0n/janken/MainActivity.kt
+++ Janken/app/src/main/java/com/first/c0tt0n/janken/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.first.c0tt0n.janken
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.choki
+import kotlinx.android.synthetic.main.activity_main.gu
+import kotlinx.android.synthetic.main.activity_main.pa
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    gu.setOnClickListener { onJankenButtonTapped(it) }
+    choki.setOnClickListener { onJankenButtonTapped(it) }
+    pa.setOnClickListener { onJankenButtonTapped(it) }
+  }
+
+  fun onJankenButtonTapped(view: View?) {
+    // TODO: start ResultActivity
   }
 }
