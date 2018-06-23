a3f6ef954f5c2cf00d85f356445e824447f0563f
yokubo <utsue.38@gmail.com>
2018-06-23 17:53:41 +0900

    generate empty project

---
+++ chapter07/AccBall/app/src/main/java/com/first/c0tt0n/accball/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.first.c0tt0n.accball
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
