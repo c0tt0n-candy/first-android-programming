3c20b984ef5415617518c06dd896fa137bdb3756
yokubo <utsue.38@gmail.com>
2018-06-29 01:28:58 +0900

    create edit screen & implement save to / delete from Realm DB

--- chapter12/MyScheduler/app/src/main/java/com/first/c0tt0n/myscheduler/MainActivity.kt
+++ chapter12/MyScheduler/app/src/main/java/com/first/c0tt0n/myscheduler/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.first.c0tt0n.myscheduler
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_main.fab
 import kotlinx.android.synthetic.main.activity_main.toolbar
 import kotlinx.android.synthetic.main.content_main.listView
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +22,13 @@ class MainActivity : AppCompatActivity() {
     val schedules = realm.where<Schedule>().findAll()
     listView.adapter = ScheduleAdapter(schedules)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+    listView.setOnItemClickListener { parent, _, position, _ ->
+      val schedule = parent.getItemAtPosition(position) as Schedule
+      startActivity<ScheduleEditActivity>("schedule_id" to schedule.id)
+    }
+
+    fab.setOnClickListener { _ ->
+      startActivity<ScheduleEditActivity>()
     }
   }
 
