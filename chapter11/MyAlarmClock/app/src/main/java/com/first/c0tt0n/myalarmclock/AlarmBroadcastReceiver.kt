071bcc28c5eaf5efb6995b03b8e3bf7f47317f00
yokubo <utsue.38@gmail.com>
2018-06-28 22:15:49 +0900

    create Dialogs and show them when users select date-time

--- chapter11/MyAlarmClock/app/src/main/java/com/first/c0tt0n/myalarmclock/AlarmBroadcastReceiver.kt
+++ chapter11/MyAlarmClock/app/src/main/java/com/first/c0tt0n/myalarmclock/AlarmBroadcastReceiver.kt
@@ -3,11 +3,14 @@ package com.first.c0tt0n.myalarmclock
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
 
   override fun onReceive(context: Context?, intent: Intent?) {
-    context?.toast("アラーム！")
+    context?.run {
+      startActivity(intentFor<MainActivity>("onReceive" to true).newTask())
+    }
   }
 }
--- chapter11/MyAlarmClock/app/src/main/java/com/first/c0tt0n/myalarmclock/MainActivity.kt
+++ chapter11/MyAlarmClock/app/src/main/java/com/first/c0tt0n/myalarmclock/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.first.c0tt0n.myalarmclock
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
@@ -7,27 +8,93 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.format.DateFormat
+import android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+import android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+import android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
 import kotlinx.android.synthetic.main.activity_main.cancelAlarm
+import kotlinx.android.synthetic.main.activity_main.dateText
 import kotlinx.android.synthetic.main.activity_main.setAlarm
+import kotlinx.android.synthetic.main.activity_main.timeText
+import org.jetbrains.anko.toast
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SimpleAlertDialog.OnClickListener, DatePickerFragment.OnDateSelectedListener, TimePickerFragment.OnTimeSelectedListener {
 
+  @Suppress("DEPRECATION")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    if (intent?.getBooleanExtra("onReceive", false) == true) {
+      when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+          window.addFlags(FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED)
+        }
+        else -> {
+          window.addFlags(FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED or FLAG_DISMISS_KEYGUARD)
+        }
+      }
+      SimpleAlertDialog().show(supportFragmentManager, "alert_dialog")
+    }
+
     setContentView(R.layout.activity_main)
 
     setAlarm.setOnClickListener {
-      val calendar = Calendar.getInstance().apply {
-        timeInMillis = System.currentTimeMillis()
-        add(Calendar.SECOND, 5)
+      val date = "${dateText.text} ${timeText.text}".toDate()
+      when {
+        date != null -> {
+          val calendar = Calendar.getInstance().apply {
+            time = date
+          }
+          setAlarmManager(calendar)
+          toast("アラームをセットしました")
+
+        }
+        else -> toast("日付の形式が正しくありません")
       }
-      setAlarmManager(calendar)
     }
 
     cancelAlarm.setOnClickListener {
       cancelAlarmManager()
     }
+
+    dateText.setOnClickListener {
+      DatePickerFragment().show(supportFragmentManager, "date_dialog")
+    }
+
+    timeText.setOnClickListener {
+      TimePickerFragment().show(supportFragmentManager, "time_dialog")
+    }
+  }
+
+  // [SimpleAlertDialog.OnClickListener]
+  override fun onPositiveClick() {
+    finish()
+  }
+
+  override fun onNegativeClick() {
+    val calendar = Calendar.getInstance().apply {
+      timeInMillis = System.currentTimeMillis()
+      add(Calendar.MINUTE, 5)
+    }
+    setAlarmManager(calendar)
+    finish()
+  }
+
+  // [DatePickerFragment.OnDateSelectedListener]
+  override fun onSelected(year: Int, month: Int, date: Int) {
+    val calendar = Calendar.getInstance().apply {
+      set(year, month, date)
+    }
+    dateText.text = DateFormat.format("yyyy/MM/dd", calendar)
+  }
+
+  // [TimePickerFragment.OnTimeSelectedListener]
+  override fun onSelected(hourOfDay: Int, minute: Int) {
+    timeText.text = "%1$02d:%2$02d".format(hourOfDay, minute)
   }
 
   private fun setAlarmManager(calendar: Calendar) {
@@ -60,5 +127,21 @@ class MainActivity : AppCompatActivity() {
         0
     )
     am.cancel(pending)
+  }
+
+  @SuppressLint("SimpleDateFormat")
+  private fun String.toDate(pattern: String = "yyyy/MM/dd HH:mm"): Date? {
+    val sdFormat = try {
+      SimpleDateFormat(pattern)
+    } catch (e: IllegalArgumentException) {
+      null
+    }
+    return sdFormat?.let {
+      try {
+        it.parse(this)
+      } catch (e: ParseException) {
+        null
+      }
+    }
   }
 }
