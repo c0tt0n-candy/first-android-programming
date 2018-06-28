db10df77b7c64a5a8a20f9a0ee363f1c1735b82a
yokubo <utsue.38@gmail.com>
2018-06-28 23:42:09 +0900

    create data access object and adapter for ListView

--- chapter12/MyScheduler/app/src/main/java/com/first/c0tt0n/myscheduler/MainActivity.kt
+++ chapter12/MyScheduler/app/src/main/java/com/first/c0tt0n/myscheduler/MainActivity.kt
@@ -3,17 +3,24 @@ package com.first.c0tt0n.myscheduler
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import io.realm.Realm
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_main.fab
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.content_main.listView
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var realm: Realm
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
+
+    realm = Realm.getDefaultInstance()
+    val schedules = realm.where<Schedule>().findAll()
+    listView.adapter = ScheduleAdapter(schedules)
 
     fab.setOnClickListener { view ->
       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -21,19 +28,8 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    menuInflater.inflate(R.menu.menu_main, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    return when (item.itemId) {
-      R.id.action_settings -> true
-      else -> super.onOptionsItemSelected(item)
-    }
+  override fun onDestroy() {
+    super.onDestroy()
+    realm.close()
   }
 }
---
+++ chapter12/MyScheduler/app/src/main/java/com/first/c0tt0n/myscheduler/ScheduleAdapter.kt
@@ -0,0 +1,43 @@
+package com.first.c0tt0n.myscheduler
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmBaseAdapter
+
+class ScheduleAdapter(data: OrderedRealmCollection<Schedule>?) : RealmBaseAdapter<Schedule>(data) {
+
+  override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    val view: View
+    val holder: ViewHolder
+
+    when (convertView) {
+      null -> {
+        val inflater = LayoutInflater.from(parent?.context)
+        view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+        holder = ViewHolder(view)
+        view.tag = holder
+      }
+      else -> {
+        view = convertView
+        holder = view.tag as ViewHolder
+      }
+    }
+
+    adapterData?.run {
+      val schedule = get(position)
+      holder.date.text = DateFormat.format("yyyy/MM/dd", schedule.date)
+      holder.title.text = schedule.title
+    }
+
+    return view
+  }
+
+  inner class ViewHolder(cell: View) {
+    val date = cell.findViewById<TextView>(android.R.id.text1)
+    val title = cell.findViewById<TextView>(android.R.id.text2)
+  }
+}
